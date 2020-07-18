391f8a731fd386d6f271136bd70fe7d5d6907744
Benjamin Stephanus <ben.stephanus@gmail.com>
2020-07-18 01:04:45 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -129,7 +129,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-     programmer_hash[:dennis_ritchie][:known_for][:languages]<< "Assembly" 
+     programmer_hash[:dennis_ritchie][:languages]<< "Assembly" 
      programmer_hash
 
 end
